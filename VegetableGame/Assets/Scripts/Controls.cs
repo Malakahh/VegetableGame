c537023a5cf4659cfae2479b201808fb3f5a04a8
Malakahh <michael_fuglsang@hotmail.com>
2015-06-25 12:50:07 +0200

    Basic movement and camera lock

--- VegetableGame/Assets/Scripts/Controls.cs
+++ VegetableGame/Assets/Scripts/Controls.cs
@@ -2,18 +2,26 @@
 using System.Collections;
 
 public class Controls : MonoBehaviour {
-    public float MovementSpeed = 1;
-
+    private float MovementSpeed = 10;
     private Vector3 movementVector = Vector3.zero;
-	
+
+    private GameObject ground;
+
+    void Awake()
+    {
+        ground = GameObject.FindGameObjectWithTag("Ground");
+    }
+
 	// Update is called once per frame
 	void Update () {
         Movement();
+        CameraMovement();
 	}
 
     void Movement()
     {
         movementVector = Vector3.zero;
+        Vector3 pos = this.transform.position;
 
         if (Input.GetKey(KeyCode.A))
         {
@@ -24,6 +32,38 @@ public class Controls : MonoBehaviour {
             movementVector.x = MovementSpeed;
         }
 
-        this.transform.position += movementVector * Time.deltaTime;
+        pos += movementVector * Time.deltaTime;
+
+        //Bounds
+        float xBound = ground.transform.localScale.x * 0.5f - this.transform.localScale.x * 0.5f;
+
+        if (pos.x < -xBound)
+        {
+            pos.x = -xBound;
+        }
+        else if (pos.x > xBound)
+        {
+            pos.x = xBound;
+        }
+
+        this.transform.position = pos;
     }
+    
+    void CameraMovement()
+    {
+        Vector3 pos = new Vector3(this.transform.position.x, this.transform.position.y + 1, -3);
+        float xBound = Mathf.Abs(ground.transform.localScale.x * 0.5f - Camera.main.orthographicSize * 3.5f * 0.5f);
+
+        if (pos.x < -xBound)
+        {
+            pos.x = -xBound;
+        }
+        else if (pos.x > xBound)
+        {
+            pos.x = xBound;
+        }
+
+        Camera.main.transform.position = pos;
+    }
+     
 }
---
+++ VegetableGame/Assets/Scripts/VegetableManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VegetableManager : MonoBehaviour {
+    
+    
+    
+    void Start()
+    {
+
+    }
+}
